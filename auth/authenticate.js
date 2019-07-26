375cd667b9e7fd323be7a644e9521f7cdb1b41fb
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-26 08:19:25 +0000

    feature: Add method to generate jwt token

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,14 +1,16 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
-const jwtKey =
-  process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+const Users = require('../config/usersModel');
+
+const jwtKey = process.env.JWT_SECRET;
 
 // quickly see what this file exports
 module.exports = {
   authenticate,
   hashPassword,
+  reversePasswordHash,
+  generateToken,
 };
 
 // implementation details
@@ -39,4 +41,35 @@ async function hashPassword(req, res, next) {
   } catch (error) {
     next(new Error('Something went wrong. Please try again.'));
   }
+}
+
+async function reversePasswordHash(req, res, next) {
+  const { username, password } = req.body;
+  try {
+    Users.findUserBy({username})
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          req.user = user;
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid credentials '});
+        }
+      })
+  } catch (error) {
+    next(new Error('Something went wrong. Please try again'));
+  }
+}
+
+function generateToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1d'
+  };
+
+  return jwt.sign(payload, jwtKey, options);
 }

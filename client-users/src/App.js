1bbf7de4aa18544a341a94491e3f74e60d9a5c41
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-26 09:37:06 +0000

    feature: Add state using useState Create base layout for the app Use react-router-dom to push user to different routes

--- client-users/src/App.js
+++ client-users/src/App.js
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
+
 import './App.css';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import ListJokes from './components/ListJokes';
+
+const authUrl = 'http://localhost:3300'
 
 function App() {
+  const [user, setUser] = useState([]);
+  const [jokes, setJokes] = useState([]);
+  const [token, setToken] = useState(false);
+  const [requestError, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="App">
+          <nav
+            style={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              margin: '20px'
+            }}
+          >
+            <NavLink
+              to="/"
+              exact
+            >
+              Jokes
+          </NavLink>
+
+            <NavLink
+              to="/sign-in"
+            >
+              SignIn
+          </NavLink>
+
+            <NavLink
+              to="/sign-up"
+            >
+              SignUp
+          </NavLink>
+          </nav>
+
+          <Route
+            path="/"
+            render={props => (
+              <ListJokes
+                {...props}
+                jokes={jokes}
+                error={requestError}
+                loading={loading}
+              />
+            )}
+          />
+
+          <Route
+            path="/sign-in"
+            render={props => (
+              <SignIn
+                {...props}
+                // signIn={signIn}
+              />
+            )}
+          />
+
+          <Route
+            path="/sign-up"
+            render={props => (
+              <SignUp
+                {...props}
+                // signUp={signUp}
+              />
+            )}
+          />
+
+        </div>
+      </Router>
     </div>
   );
 }
